cdf629ce9f2fe7c6226638261003bee1d458450d
Joshua Ngugi <joshuamwaurangugi@gmail.com>
2025-05-20 13:18:04 +0300

    add user roles

--- turuke_app/lib/constants.dart
+++ turuke_app/lib/constants.dart
@@ -1,3 +1,28 @@
 class Constants {
   static const String API_BASE_URL = 'http://localhost:3000/api';
 }
+
+class UserRole {
+  static const int ADMIN = 1;
+  static const int MANAGER = 2;
+  static const int SUPERVISOR = 3;
+  static const int ASSISTANT = 4;
+  static const int VIEWER = 5;
+
+  String get getString {
+    switch (this) {
+      case ADMIN:
+        return 'Admin';
+      case MANAGER:
+        return 'Manager';
+      case SUPERVISOR:
+        return 'Supervisor';
+      case ASSISTANT:
+        return 'Assistant';
+      case VIEWER:
+        return 'Viewer';
+      default:
+        return 'Admin';
+    }
+  }
+}
